c920194bfe1029114dda23d912559c3ed3aa8810
hzethraeus <hzethraeus@gmail.com>
2022-11-10 14:42:55 +0100

    Image loaded from api

--- src/PlayerPage.tsx
+++ src/PlayerPage.tsx
@@ -2,56 +2,8 @@ import styles from './styles/PlayerPage.module.css';
 import PlayerCard from './components/PlayerCard';
 //import { useQuery, gql } from '@apollo/client';
 import { Player, useGetPlayersQuery } from './graphql/generated-types';
-interface PlayersProps {}
 
-/*interface PlayerInfo {
-  name: string;
-  clan: string;
-  country: string;
-  playerData: {
-    playerId: string;
-    clanId: string;
-  };
-
-  playerStats: {
-    ovr: number;
-    acc: number;
-    imp: number;
-    ast: number;
-    ent: number;
-    utl: number;
-    exp: number;
-  };
-}
-
-interface PlayerArray {
-  players: Player[];
-}
-*/
-/*
-const GET_PLAYER_QUERY = gql`
-  query GetPlayers {
-    players {
-      name
-      clan
-      country
-      playerData {
-        playerId
-        clanId
-      }
-      playerStats {
-        ovr
-        acc
-        imp
-        ast
-        ent
-        utl
-        exp
-      }
-    }
-  }
-`;*/
-const PlayerPage: React.FunctionComponent<PlayersProps> = () => {
+const PlayerPage = () => {
   const { loading, data } = useGetPlayersQuery(); //useQuery<PlayerArray>(GET_PLAYER_QUERY);
   console.log(data);
   return (
